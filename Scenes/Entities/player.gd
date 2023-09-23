65c4c2cf02e867f4015cc9970496d382f42450c9
Plinio Productions <plinio.jrm@gmail.com>
2023-09-22 21:05:21 -0300

    Recreating Player scene

--- Scenes/Entities/player.gd
+++ Scenes/Entities/player.gd
@@ -1,7 +1,44 @@
-extends Node2D
+extends RigidBody2D
+
+@export var acceleration: int = 300
+var move_input: int = 0
+var rotation_input: int = 0
+@export var current_rotation: float
 
 func _input(event):
 	if event.is_action_pressed("speed_up"):
-		print("accelerate")
+		move_input = 1
 	elif event.is_action_pressed("speed_down"):
-		print("Deaccelerate")
+		move_input = -1
+	else:
+		move_input = 0
+
+	if event.is_action_pressed("turn_left"):
+		rotation_input = -1
+	elif event.is_action_pressed("turn_right"):
+		rotation_input = 1
+	else:
+		rotation_input = 0
+
+func _process(delta):
+	current_rotation = rotation
+
+func _physics_process(delta):
+	Process_Rotation(delta)
+	#Process_Movement()
+	return
+
+func Process_Movement():
+	if move_input == 0:
+		return
+	
+	var direction_acceleration = Vector2(0, acceleration * move_input)
+	print(direction_acceleration)
+	var velocity = direction_acceleration.rotated(rotation)
+	print(velocity)
+	apply_force(velocity)
+	
+func Process_Rotation(delta):
+	rotation += rotation_input * delta
+	#rotate(rotation_input * delta)
+	#apply_torque_impulse(rotation_input)
